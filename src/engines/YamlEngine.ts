752d0e638e86c7e20bbdb05249cd673afada5571
Chris Tate <chris@austincodeshop.com>
2019-07-11 20:22:01 -0500

    adds options

--- src/engines/YamlEngine.ts
+++ src/engines/YamlEngine.ts
@@ -2,5 +2,5 @@ import { IEngine } from '@zappjs/core';
 import { safeDump } from 'js-yaml';
 
 export const YamlEngine: IEngine = (spec) => {
-  return safeDump(spec);
+  return safeDump(spec, { noRefs: true, lineWidth: 250 });
 }
